b3d9b18d9cb21d23e7d605edf29b2fe3f8d87405
monesh <moneshd16@gmail.com>
2025-04-24 20:20:58 +0530

    fix : UI

--- crud/create.php
+++ crud/create.php
@@ -15,13 +15,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <title>Add User</title>
     <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+
 </head>
 <body>
 <h2>Add New User</h2>
-<form method="POST">
+<!-- <form method="POST">
     <input type="text" name="name" placeholder="Name" required><br>
     <input type="email" name="email" placeholder="Email" required><br>
     <button type="submit">Add</button>
+</form> -->
+<form  method="POST">
+  <div class="mb-3">
+    <label for="email" class="form-label">Email address</label>
+    <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="name" class="form-label">Name</label>
+    <input type="text" class="form-control" name="name" id="name">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 <a href="index.php">Back</a>
 </body>
--- crud/edit.php
+++ crud/edit.php
@@ -18,15 +18,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <title>Edit User</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+
 </head>
 
 <body>
     <h2>Edit User</h2>
-    <form method="POST">
+    <!-- <form method="POST">
         <input type="text" name="name" value="<?= $user['name'] ?>" required><br>
         <input type="email" name="email" value="<?= $user['email'] ?>" required><br>
         <button type="submit">Update</button>
-    </form>
+    </form> -->
+    <form  method="POST">
+  <div class="mb-3">
+    <label for="email" class="form-label">Email address</label>
+    <input type="email" class="form-control" name="email" value="<?=$user['email'] ?> " id="email" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="name" class="form-label">Name</label>
+    <input type="text" class="form-control" name="name" value="<?= $user['email'] ?>" id="name">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     <a href="index.php">Back</a>
 </body>
 
